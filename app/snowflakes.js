6152da0f819f3f96eda22d61bada8f20d0afa0de
Krishna Glick <unstoppable411@gmail.com>
2016-12-06 15:08:04 -0500

    bondsmith sutff

--- app/snowflakes.js
+++ app/snowflakes.js
@@ -1,6 +1,7 @@
 
 const snowflakes = {
-  LordMistborn: `We trade one lord ruler for another <@id>`
+  LordMistborn: `We trade one Lord Ruler for another <@id>`,
+  eviljer: `Welcome <@id>, you have a hard road in front of you.`
 };
 
 module.exports = function(user) {
