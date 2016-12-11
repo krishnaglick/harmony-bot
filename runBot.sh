0b47f0e810e450d1afb778e8ae36de7fbd242cfd
Krishna Glick <unstoppable411@gmail.com>
2016-12-11 18:45:43 -0500

    whyyyy

--- runBot.sh
+++ runBot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ssh 107.170.78.145 -l root << 'ENDSSH'
 cd ~/harmony-bot
+git reset --hard
 git pull origin-ssh master
 yarn
 docker ps -a | awk '{ print $1,$2 }' | grep harmonybot_harmony | awk '{print $1 }' | xargs -I {} docker rm {} -f
